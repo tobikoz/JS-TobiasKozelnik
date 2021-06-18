76ba302518a9909f06edde9c1baeab1040eb88b1
tobikoz <84669693+tobikoz@users.noreply.github.com>
2021-06-18 17:01:59 -0300

    entrega 1

---
+++ claseUno.js
@@ -0,0 +1,20 @@
+const nombre = prompt("Ingrese su nombre");
+
+const apellido = prompt("Ingrese su apellido");
+
+const edad = prompt("Ingrese su edad");
+
+const telefono =  prompt("Ingrese su numero de telefono");
+
+alert("Su nombre y apellido es " + nombre + " " + apellido + " su edad es " + edad + " y su telefono es " + telefono + ". Muchas gracias por confiar en nosotros!");
+
+alert("Ahora vamos a realizar una suma simple")
+
+
+const numeroUno =  parseInt(prompt("Ingrese un numero para sumar"));
+
+const numeroDos=  parseInt(prompt("Ingrese un numero para sumar"));
+
+const sumaNumero= numeroUno + numeroDos; 
+
+alert("La suma de los dos numeros es " + sumaNumero);
