1a079f523e18b132a8de892a9b9fab99c8e65fe0
tobikoz <84669693+tobikoz@users.noreply.github.com>
2021-06-27 15:39:40 -0300

    TP numero 3

---
+++ Clase3Kozelnik/clase3.js
@@ -0,0 +1,40 @@
+let entrada = prompt("Por favor ingrese un numero del 1 al 10")
+
+while(entrada !="ESC"){
+    switch (entrada) {
+        case "1":
+            alert("Ingreso un numero impar");
+            break;
+        case "2":
+            alert("Ingreso un numero par");
+            break;
+        case "3":
+            alert("Ingreso un numero impar");
+            break;
+        case "4":
+            alert("Ingreso un numero par");
+            break;   
+        case "5":
+            alert("Ingreso un numero impar");
+            break;
+        case "6":
+            alert("Ingreso un numero par");
+            break;
+        case "7":
+            alert("Ingreso un numero impar");
+            break;
+        case "8":
+            alert("Ingreso un numero par");
+            break; 
+        case "9":
+            alert("Ingreso un numero impar");
+            break;
+        case "10":
+            alert("Ingreso un numero par");
+            break; 
+        default:
+            alert(" ERROR!!! Ingrese un numero del 1 al 10");
+            break;
+}
+entrada = prompt("Por favor ingresa un numero del 1 al 10");
+} 
